e67205bd1a766aae646129d47680d42e7d67477a
nghung <nghung28102007@gmail.com>
2025-03-30 14:17:42 +0700

    cap nhat

---
+++ interface/script.js
@@ -0,0 +1,15 @@
+//Dropdown menu
+const dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('active');
+});
+
+function signIn() {
+    // Removed showModal call
+}
+
+function signUp() {
+    // Removed showModal call
+}
+
